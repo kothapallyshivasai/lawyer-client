0816755e91225aa598bf349423cb43e10f433cfc
kothapallyshivasai <shivasaikothapally@gmail.com>
2024-10-23 02:46:33 +0530

    Done

--- client/models.py
+++ client/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
 class Booking(models.Model):
     id = models.AutoField(primary_key=True)
     lawyer = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -9,6 +9,7 @@ class Booking(models.Model):
     phone_number = models.CharField(max_length=10)
     status = models.BooleanField()
     case_type = models.TextField()
+    date_time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.lawyer.username} - {self.client}"    
